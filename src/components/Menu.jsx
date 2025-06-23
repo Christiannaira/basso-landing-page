ea73fdd5c260c0487b230b3eda1e8fde295e1a76
Christian <Christianmnaira@gmail.com>
2025-06-23 22:24:05 +0800

    feat: added background image, set up title and initialize menu component contents

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import Button from "./Button";
+import menu from "../assets/menu.jpg";
 const Menu = () => {
-  return (
-    <div>
-      menu page
-    </div>
-  )
-}
+   return (
+      <div className="menu__content">
+         <div className="menu__content--headline">
+            <h2>
+               Tired of the same <span>boring brews</span> <br /> or sugary
+               drinks that <span>don’t satisfy</span>?{" "}
+            </h2>
+         </div>
 
-export default Menu
+         <div className="menu__content--image">
+            <img src={menu} alt="menu-image" />
+         </div>
+
+         <p>
+            Our menu blends bold, comforting coffee with refreshing, non-coffee
+            options <br /> made to match your vibe — morning, noon, or night.{" "}
+         </p>
+
+         <p>Because you deserve a drink that fits you.</p>
+
+         <Button text={"Follow us for your daily drink & fresh drops!"} />
+      </div>
+   );
+};
+
+export default Menu;
