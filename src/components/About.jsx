dd23e81eed30b3a3e24cd49bc23698de97fe9e67
Christian <Christianmnaira@gmail.com>
2025-06-24 22:15:05 +0800

    feat: initialize about component contents

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
 
 const About = () => {
-  return (
-    <div className='about__content'>
+   return (
+      <div className="about__content">
+         <div className="about__content--headline">
+            <h2>Meet Our Team</h2>
+         </div>
 
-      <div className="about__content--headline">
-        <h2>Meet Our Team</h2>
-      </div>
-      
-      <div className="about__content--image">
-        <img src="" alt="" />
-      </div>
-      
-    </div>
-  )
-}
+         <div className="about__content--image">
+            <img src="" alt="" />
+         </div>
 
-export default About
+         <div className="about__contnet--title">
+            <h3>More Than Just Coffee. It's Mood.</h3>
+         </div>
+
+         <div className="about__content--row">
+            <p>
+               We started with a simple question: Why should great taste belong
+               only to coffee lovers?
+            </p>
+         </div>
+
+         <div className="about__content--row">
+            <p>
+               At Basso, we blend bold coffee flavors with refreshing, creative
+               non-coffee drinks — because we believe everyone deserves
+               something that fits their vibe. Whether you're craving a creamy
+               espresso, a chill matcha, or a fruity cooler that hits just
+               right, we’ve got something brewed for you.
+            </p>
+         </div>
+
+        <div className="about__content--row">
+            <p>We’re not a big chain — we’re a passion project. Built for people who want quality, personality, and something a little different with every sip. Our drinks are handcrafted with care, and our goal is simple: make your everyday drink feel like a small moment of joy.</p>
+        </div>
+
+        <div className="about__content--row">
+            <p>So whether you're here for the caffeine or just here for the flavor — welcome. You're one of us.</p>
+        </div>
+
+        <Button text={"Follow Us. Sip With Us"}/>
+
+      </div>
+   );
+};
+
+export default About;
