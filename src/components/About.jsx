62939fe2a5c88b7b3b3b62b1423426445e64e50b
Christian <Christianmnaira@gmail.com>
2025-06-24 22:26:10 +0800

    feat: designed the about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import team from "../assets/hero_team.jpg";
 
 const About = () => {
    return (
@@ -9,7 +10,7 @@ const About = () => {
          </div>
 
          <div className="about__content--image">
-            <img src="" alt="" />
+            <img src={team} alt="team-image" />
          </div>
 
          <div className="about__contnet--title">
@@ -18,27 +19,27 @@ const About = () => {
 
          <div className="about__content--row">
             <p>
-               We started with a simple question: Why should great taste belong
-               only to coffee lovers?
+               We started with a simple question: <br /><span>Why should great taste belong
+               only to coffee lovers?</span>
             </p>
          </div>
 
          <div className="about__content--row">
             <p>
-               At Basso, we blend bold coffee flavors with refreshing, creative
-               non-coffee drinks — because we believe everyone deserves
-               something that fits their vibe. Whether you're craving a creamy
-               espresso, a chill matcha, or a fruity cooler that hits just
+               At <span>Basso</span>, we blend bold coffee flavors with refreshing, creative
+               non-coffee drinks because we believe everyone deserves
+               something that fits their vibe. Whether you're craving a <span>creamy
+               espresso</span>, a <span>chill matcha</span>, or a <span>fruity cooler</span> that hits just
                right, we’ve got something brewed for you.
             </p>
          </div>
 
         <div className="about__content--row">
-            <p>We’re not a big chain — we’re a passion project. Built for people who want quality, personality, and something a little different with every sip. Our drinks are handcrafted with care, and our goal is simple: make your everyday drink feel like a small moment of joy.</p>
+            <p>We’re not a big chain, we’re a passion project. <b>Built for people who want quality, personality, and something a little different with every sip</b>. Our drinks are handcrafted with care, and our goal is simple: make your everyday drink feel like a small moment of joy.</p>
         </div>
 
         <div className="about__content--row">
-            <p>So whether you're here for the caffeine or just here for the flavor — welcome. You're one of us.</p>
+            <p>So whether you're here for the caffeine or just here for the flavor, welcome. <br /><span>You're one of us.</span></p>
         </div>
 
         <Button text={"Follow Us. Sip With Us"}/>
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <div className='gallery__content'>
+      
+        <div className="gallery__content--headline">
+            <h2>Our Beloved Customers</h2>
+        </div>
+
+    </div>
+  )
+}
+
+export default Gallery
