47ed03f9466c9f90c0ab00be91a6923951daac99
Christian <Christianmnaira@gmail.com>
2025-07-05 21:40:19 +0800

    fix: web responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import Button from "./Button";
 import hero from "../assets/hero_image.jpg";
 import logo from "../assets/logo.png";
+import drink1 from "../assets/drink1.png";
+import drink2 from "../assets/drink2.png";
 
 const Hero = () => {
    return (
@@ -23,6 +25,9 @@ const Hero = () => {
         <div className="hero__content--image">
             <img src={hero} alt="basso-hero-image" />
         </div>
+
+        <img src={drink1} alt="drink icon" className="drink_icon icon1"/>
+        <img src={drink2} alt="drink icon" className="drink_icon icon2"/>
 
       </div>
    );
